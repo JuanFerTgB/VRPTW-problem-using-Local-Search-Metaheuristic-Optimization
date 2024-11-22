5c392e670d64090d540593bb8a99c9e443d40b92
Juan Fernando Riascos Goyes <160544309+JuanFerTgB@users.noreply.github.com>
2024-11-22 01:43:03 -0500

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-def select_parents(population, fitness_values):
-    """
-    Selecciona a los padres utilizando el método de ruleta o selección por torneo.
-    """
-    total_fitness = sum(fitness_values)
-    selection_probs = [fitness / total_fitness for fitness in fitness_values]
-    parents = random.choices(population, weights=selection_probs, k=2)  # Selecciona 2 padres
-    return parents
